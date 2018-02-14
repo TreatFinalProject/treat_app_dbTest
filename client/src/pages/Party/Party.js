843b1f51b14fb2bb70cc2d77cd873a366ae57f4a
kelly quinn <kellyquinn213@gmail.com>
2018-02-14 14:30:53 -0500

    layout

--- client/src/pages/Party/Party.js
+++ client/src/pages/Party/Party.js
@@ -56,53 +56,53 @@ class Party extends React.Component {
         let local = ""
         let date = ""
 
-    for (var i = 0; i < this.state.events.length; i++) {
-        if (this.state.events.length > 0) {
-            party = this.state.events[i].eventName
+        for (var i = 0; i < this.state.events.length; i++) {
+            if (this.state.events.length > 0) {
+                party = this.state.events[i].eventName
+            }
+            if (this.state.events.length > 0) {
+                host = this.state.events[i].eventHost
+            }
+            if (this.state.events.length > 0) {
+                local = this.state.events[i].location
+            }
+            if (this.state.events.length > 0) {
+                date = this.state.events[i].date
+            }
         }
-        if (this.state.events.length > 0) {
-            host = this.state.events[i].eventHost
-        }
-        if (this.state.events.length > 0) {
-            local = this.state.events[i].location
-        }
-        if (this.state.events.length > 0) {
-            date = this.state.events[i].date
-        }
-    }
-    
-    return(
-        
+
+        return (
+
             <div>
-            <Container style={{ marginTop: 30 }}>
-                <Row>
-                    {/* Dash Column */}
-                    {/* <Col size="md-3">
+                <Container style={{ marginTop: 30 }}>
+                    <Row>
+                        {/* Dash Column */}
+                        <Col size="md-3">
                             <h1 className="text-left">Dashboard</h1>
                             <ul className="list-group list-group-flush">
                                 <a href="/party/todo" className="list-group-item list-group-item-action">To Do</a>
                                 <a href="/party/guestlist" className="list-group-item list-group-item-action">Guest List</a>
                             </ul>
-                        </Col> */}
-                    {/* Event Column */}
-                    <Col size="md-12">
-                        <Jumbotron>
-                            <h1 className="text-center">{party}</h1>
-                        </Jumbotron>
-                        {/* Info Row */}
-                        <Row>
-                            <Col size="md-3">
-                                <Images />
-                                <div className="petPic"></div>
-                            </Col>
-                            <Col size="md-9">
-                                <h2> Date: {date}</h2>
-                                <h2> Location: {local}</h2>
-                                <h2> Host: {host}</h2>
-                            </Col>
+                        </Col>
+                        {/* Event Column */}
+                        <Col size="md-9">
+                            <Jumbotron>
+                                <h1 className="text-center">{party}</h1>
+                            </Jumbotron>
+                            {/* Info Row */}
+                            <Row>
+                                <Col size="md-3">
+                                    <Images />
+                                    <div className="petPic"></div>
+                                </Col>
+                                <Col size="md-9">
+                                    <h2> Date: {date}</h2>
+                                    <h2> Location: {local}</h2>
+                                    <h2> Host: {host}</h2>
+                                </Col>
 
-                        </Row>
-                        {/* <Row>
+                            </Row>
+                            {/* <Row>
                                 <Col size="md-5">
                                     {Todo}
                                 </Col>
@@ -112,16 +112,16 @@ class Party extends React.Component {
                                 </Col>
                             </Row> */}
 
-                        {/* Changing Dash Component Row */}
-                        {/* <Row>
+                            {/* Changing Dash Component Row */}
+                            {/* <Row>
                                 <Route exact path={"/party/todo"} component={Todo} />
                                 <Route exact path={`/party/guestlist`} component={GuestList} />
                             </Row> */}
-                    </Col>
-                </Row>
-            </Container>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
-}
+    }
 }
 export default Party;
