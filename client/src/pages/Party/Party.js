ba65f8932d57a6cec725fa1f4492ff699c1ac1d4
kelly quinn <kellyquinn213@gmail.com>
2018-02-14 14:19:29 -0500

    Party.js populating BUT only at position[0]

--- client/src/pages/Party/Party.js
+++ client/src/pages/Party/Party.js
@@ -24,65 +24,85 @@ class Party extends React.Component {
             events: [],
             event: {},
             email: "",
-            unique:[]
+            unique: []
         };
     }
 
     componentDidMount() {
         firebase.auth.onAuthStateChanged(authUser => {
-          this.setState({"email": authUser.email})
-          this.loadEvents();
+            this.setState({ "email": authUser.email })
+            this.loadEvents();
         })
-      }
-    
-      // Loads all books  and sets them to this.state.books
-      loadEvents = user => {
+    }
+
+    // Loads all books  and sets them to this.state.books
+    loadEvents = user => {
         API.getEvents(this.state.email)
-          .then(res =>this.setState({ events: res.data}))
-          .catch(err => console.log(err));  
+            .then(res => this.setState({ events: res.data }))
+            .catch(err => console.log(err));
     };
 
-// When this component mounts, grab the event with the _id of this.props.match.params.id
-  componentwillMount() {
-    API.getEvent(this.props.match.params.id)
-      .then(res => this.setState({ event: res.data }))
-      .catch(err => console.log(err));
-  };
+    // When this component mounts, grab the event with the _id of this.props.match.params.id
+    componentwillMount() {
+        API.getEvent(this.props.match.params.id)
+            .then(res => this.setState({ event: res.data }))
+            .catch(err => console.log(err));
+    };
 
     render() {
+
+        let party = ""
+        let host = ""
+        let local = ""
+        let date = ""
+
+    for (var i = 0; i < this.state.events.length; i++) {
+        if (this.state.events.length > 0) {
+            party = this.state.events[i].eventName
+        }
+        if (this.state.events.length > 0) {
+            host = this.state.events[i].eventHost
+        }
+        if (this.state.events.length > 0) {
+            local = this.state.events[i].location
+        }
+        if (this.state.events.length > 0) {
+            date = this.state.events[i].date
+        }
+    }
     
-        return (
+    return(
         
             <div>
-                <Container style={{ marginTop: 30 }}>
-                    <Row>
-                        {/* Dash Column */}
-                        {/* <Col size="md-3">
+            <Container style={{ marginTop: 30 }}>
+                <Row>
+                    {/* Dash Column */}
+                    {/* <Col size="md-3">
                             <h1 className="text-left">Dashboard</h1>
                             <ul className="list-group list-group-flush">
                                 <a href="/party/todo" className="list-group-item list-group-item-action">To Do</a>
                                 <a href="/party/guestlist" className="list-group-item list-group-item-action">Guest List</a>
                             </ul>
                         </Col> */}
-                        {/* Event Column */}
-                        <Col size="md-12">
+                    {/* Event Column */}
+                    <Col size="md-12">
                         <Jumbotron>
-                            <h1 className="text-center">{this.state.event.eventName}</h1>
-                            </Jumbotron>
-                            {/* Info Row */}
-                            <Row>
+                            <h1 className="text-center">{party}</h1>
+                        </Jumbotron>
+                        {/* Info Row */}
+                        <Row>
                             <Col size="md-3">
-                                    <Images />
-                                    <div className="petPic"></div>
-                                </Col>
-                                <Col size="md-9">
-                                    <h2> Date: {this.state.event.date}</h2>
-                                    <h2> Location: {this.state.event.location}</h2>
-                                    <h2> Host: {this.state.event.eventHost}</h2>
-                                </Col>
-                                
-                            </Row>
-                            {/* <Row>
+                                <Images />
+                                <div className="petPic"></div>
+                            </Col>
+                            <Col size="md-9">
+                                <h2> Date: {date}</h2>
+                                <h2> Location: {local}</h2>
+                                <h2> Host: {host}</h2>
+                            </Col>
+
+                        </Row>
+                        {/* <Row>
                                 <Col size="md-5">
                                     {Todo}
                                 </Col>
@@ -91,17 +111,17 @@ class Party extends React.Component {
                                     {GuestList}
                                 </Col>
                             </Row> */}
-                            
-                            {/* Changing Dash Component Row */}
-                            {/* <Row>
+
+                        {/* Changing Dash Component Row */}
+                        {/* <Row>
                                 <Route exact path={"/party/todo"} component={Todo} />
                                 <Route exact path={`/party/guestlist`} component={GuestList} />
                             </Row> */}
-                        </Col>
-                    </Row>
-                </Container>
+                    </Col>
+                </Row>
+            </Container>
             </div>
         );
-    }
+}
 }
 export default Party;
